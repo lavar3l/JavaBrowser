8563f8bcd43510a05d08f66d52a2fb415e9c0af9
Przemysław Dominikowski <przemyslaw.dominikowski@intel.com>
2021-12-25 12:51:45 +0100

    Add tab view
    Created tab view with:
    * adding new tab,
    * reordering tabs,
    * closing selected tab.
    
    Each tab has dedicated WebEngine and WebView components, but shares rest of the UI (address bar, control buttons, etc.).

--- src/main/java/com/browser/javabrowser/BrowserController.java
+++ src/main/java/com/browser/javabrowser/BrowserController.java
@@ -1,37 +1,124 @@
 package com.browser.javabrowser;
 
+import com.browser.javabrowser.settings.Settings;
+import com.browser.javabrowser.tabs.BrowserTab;
+import com.browser.javabrowser.tools.URLSanitizer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebHistory;
-import javafx.scene.web.WebView;
 
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class BrowserController implements Initializable {
-    @FXML
-    private WebView webView;
-
+public class BrowserController implements Initializable, IBrowsable {
     @FXML
     private TextField textField;
 
-    private WebEngine engine;
+    @FXML
+    private TabPane tabPane;
+
+    private List<BrowserTab> tabs;
+    private BrowserTab activeTab;
 
     @Override
-    public void initialize(URL arg0, ResourceBundle arg1)
-    {
-        engine = webView.getEngine();
-        String homePage = "www.google.com";
-        textField.setText(homePage);
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // Set tab pane policies
+        this.tabPane.setTabDragPolicy(TabPane.TabDragPolicy.REORDER);
+        this.tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.ALL_TABS);
+
+        // Prepare tab collection and initial tab
+        this.tabs = new ArrayList<BrowserTab>();
+        this.addNewTab(null);
+        this.activeTab = this.tabs.get(0);
+        this.tabPane.getTabs().add(newTabButton(this.tabPane));
+        this.tabPane.getSelectionModel().selectedItemProperty().addListener(
+                (ov, t, t1) -> this.onTabSelectionChange()
+        );
+    }
+
+    private Tab newTabButton(TabPane tabPane) {
+        Tab newTabButton = new Tab("+");
+        newTabButton.setClosable(false);
+        tabPane.getSelectionModel().selectedItemProperty().addListener((observable, oldTab, activeTab) -> {
+            if(activeTab == newTabButton) {
+                // Add and select new tab
+                this.addNewTab(null);
+
+                // Add new tab button at the end of the tab pane
+                this.tabPane.getTabs().remove(newTabButton);
+                this.tabPane.getTabs().add(newTabButton);
+            }
+        });
+        return newTabButton;
     }
 
     public void loadPage(ActionEvent actionEvent) {
-        engine.load("http://"+textField.getText());
+        this.navigateURL(URLSanitizer.getFullURL(this.textField.getText()));
+    }
 
+    public void loadHomePage(ActionEvent actionEvent) {
+        this.navigateURL(Settings.homePage);
+    }
+
+    public void addNewTab(ActionEvent actionEvent) {
+        BrowserTab browserTab = new BrowserTab(Settings.homePage, this);
+        this.tabs.add(browserTab);
+        this.tabPane.getTabs().add(browserTab.getFXTab());
+        this.tabPane.getSelectionModel().select(browserTab.getFXTab());
+    }
+
+    private BrowserTab getActiveTab() {
+        Tab activeTab = this.tabPane.getSelectionModel().getSelectedItem();
+        for(BrowserTab browserTab : this.tabs)
+        {
+            if(activeTab == browserTab.getFXTab())
+            {
+                return browserTab;
+            }
+        }
+        return null;
+    }
+
+
+
+    @Override
+    public void navigateForward() {
+        this.activeTab.navigateForward();
+    }
+
+    @Override
+    public void navigateBackward() {
+        this.activeTab.navigateBackward();
+    }
+
+    @Override
+    public void navigateURL(String url) {
+        this.activeTab.navigateURL(url);
+    }
+
+    @Override
+    public void reloadPage() {
+        this.activeTab.reloadPage();
+    }
+
+    public void changeAddressText(String url, Integer tabId) {
+        if(tabId == null || tabId.equals(this.activeTab.getId()))
+        {
+            this.textField.setText(url);
+        }
+    }
+
+    public void onTabSelectionChange()
+    {
+        this.activeTab = this.getActiveTab();
+        this.changeAddressText(this.activeTab.getURL(), null);
     }
 }
---
+++ src/main/java/com/browser/javabrowser/tabs/BrowserTab.java
@@ -0,0 +1,85 @@
+package com.browser.javabrowser.tabs;
+
+import com.browser.javabrowser.BrowserController;
+import com.browser.javabrowser.IBrowsable;
+import com.browser.javabrowser.tools.URLSanitizer;
+import javafx.scene.control.Tab;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebHistory;
+import javafx.scene.web.WebView;
+
+public class BrowserTab implements IBrowsable {
+    private static Integer tabCount = 0;
+    private Integer id;
+    private Tab fxTab;
+    private WebView webView;
+    private WebEngine engine;
+    private BrowserController controller;
+
+    public BrowserTab(String url, BrowserController controller) {
+        this.id = ++BrowserTab.tabCount;
+        this.controller = controller;
+        this.fxTab = new Tab(URLSanitizer.getTabTitle(url));
+        this.webView = new WebView();
+        this.engine = this.webView.getEngine();
+        this.engine.load(url);
+        this.fxTab.setContent(this.webView);
+
+        this.engine.getLoadWorker().stateProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    this.onPageChange();
+                });
+    }
+
+    public Tab getFXTab() {
+        return this.fxTab;
+    }
+
+    @Override
+    public void navigateForward() {
+        this.navigateHistory(1);
+    }
+
+    @Override
+    public void navigateBackward() {
+        this.navigateHistory(-1);
+    }
+
+    @Override
+    public void navigateURL(String url) {
+        this.engine.load(url);
+    }
+
+    @Override
+    public void reloadPage() {
+        this.engine.reload();
+    }
+
+    private void onPageChange() {
+        String currentURL = this.getURL();
+        System.out.println(currentURL);
+        this.fxTab.setText(URLSanitizer.getTabTitle(currentURL));
+        this.controller.changeAddressText(currentURL, this.id);
+        // TO DO: send history entry to History class
+    }
+
+    private void navigateHistory(int step) {
+        WebHistory history = engine.getHistory();
+        int newIndex = history.getCurrentIndex() + step;
+        if(newIndex >= 0 && newIndex < history.getEntries().size())
+        {
+            history.go(step);
+            this.onPageChange();
+        }
+    }
+
+    public String getURL()
+    {
+        return this.engine.getLocation();
+    }
+
+    public Integer getId()
+    {
+        return this.id;
+    }
+}
